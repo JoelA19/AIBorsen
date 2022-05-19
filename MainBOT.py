3d83dde981031fdbf5b830344fe5365e5224380d
ABBJamMon <54663152+ABBJamMon@users.noreply.github.com>
2022-05-19 14:39:04 +0200

    Bajs

--- MainBOT.py
+++ MainBOT.py
@@ -1,17 +1,22 @@
-from asyncio import tasks
-from enum import auto
-from discord import File
-from discord.ext import tasks, commands
-from tracemalloc import start
-from data import stocks
 import discord
 import ctx
 import os
+from asyncio import tasks
+from enum import auto
+from lib2to3.pgen2.token import NEWLINE
+from re import S
+from discord import File
+from discord.ext import tasks, commands
+from tracemalloc import start
+from matplotlib.pyplot import title
+from data import stocks
+
 
 bot = commands.Bot("!")
 
-target_channel_id = ('Text channel ID')
+target_channel_id = 973478588265406524
 #Höger klicka på text kanal sedan copy channel ID
+
 
 @tasks.loop(hours=12)
 async def called_once_a_day():
@@ -21,7 +26,7 @@ async def called_once_a_day():
         if stocks[stock][-1] > 70:
             embed = discord.Embed(
             title=stock, description=('Sell ' + stock + ', RSI: ' + str(int(stocks[stock][-1]))),
-            colour=discord.Colour.purple()
+            colour=discord.Colour.dark_gray()
             )
             await message_channel.send(embed=embed, file=discord.File("pictures/" + stock + ".png"))
 
@@ -30,7 +35,7 @@ async def called_once_a_day():
                 title=stock,
                 description=('Buy ' + stock + ', RSI: ' +
                              str(int(stocks[stock][-1]))),
-                colour=discord.Colour.purple()
+                colour=discord.Colour.dark_gray()
             )
             await message_channel.send(embed=embed, file=discord.File("pictures/" + stock + ".png"))
 
@@ -41,5 +46,5 @@ async def before():
     print("Finished waiting")
 
 called_once_a_day.start()
-bot.run("Discord Bot Token")
+bot.run("token")
 #använd https://discord.com/developers/applications/ för att skapa en bot för att sedan gå undder bot och hämta token genom reset token
--- Testbot.py
+++ Testbot.py
@@ -1,8 +1,11 @@
 from asyncio import tasks
 from enum import auto
+from lib2to3.pgen2.token import NEWLINE
+from re import S
 from discord import File
 from discord.ext import tasks, commands
 from tracemalloc import start
+from matplotlib.pyplot import title
 from data import stocks
 import discord
 import ctx
@@ -10,6 +13,38 @@ import os
 
 
 client = discord.Client()
+client = commands.Bot("$")
+target_channel_id = 973478588265406524
+
+@tasks.loop(hours=12)
+async def called_once_a_day():
+    message_channel = client.get_channel(target_channel_id)
+    print(f"Got channel {message_channel}")
+    for stock in stocks:
+        if stocks[stock][-1] > 70:
+            embed = discord.Embed(
+                title=stock, description=(
+                    'Sell ' + stock + ', RSI: ' + str(int(stocks[stock][-1]))),
+                colour=discord.Colour.dark_gray()
+            )
+            await message_channel.send(embed=embed, file=discord.File("pictures/" + stock + ".png"))
+
+        elif stocks[stock][-1] < 30:
+            embed = discord.Embed(
+                title=stock,
+                description=('Buy ' + stock + ', RSI: ' +
+                             str(int(stocks[stock][-1]))),
+                colour=discord.Colour.dark_gray()
+            )
+            await message_channel.send(embed=embed, file=discord.File("pictures/" + stock + ".png"))
+
+
+@called_once_a_day.before_loop
+async def before():
+    await client.wait_until_ready()
+    print("Finished waiting")
+
+called_once_a_day.start()
 
 
 @client.event
@@ -19,34 +54,43 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    med = ''
+    med2 = ''
     if message.author == client.user:
         return
-    if message.content.startswith('$test'):
-        for stock in stocks:
-            if stocks[stock][-1] > 70:
-                embed = discord.Embed(
-                    title=stock,
-                    description=('Sell ' + stock + ', RSI: ' + str(int(stocks[stock][-1]))),
-                    colour=discord.Colour.purple()
-                )
-                await message.channel.send(embed=embed, file=discord.File("pictures/" + stock + ".png"))
+    for stock in stocks:
+        med += str(stock) + '\n'
+    if message.content.startswith('$stocklist'):
+        embed2 = discord.Embed(
+        title = 'Stock List',
+        description = med)
+        await message.channel.send(embed=embed2) 
+    if message.content.startswith('$stock'):
+        name = message.content.split(' ')
+        name = name[1]
+        V1 = stocks[name]
+        name= str(name)
+        X = ''
+        # for V in V1:
+        k = 0
+        while k < 3:
+            X += str(V1[-k]) + '\n'
+            k += 1
+        embed2 = discord.Embed(
+        title=name,
+        description = "RSI:" '\n' + X)
+        colour = discord.Colour.dark_gray()
+        await message.channel.send(embed=embed2)
 
-            elif stocks[stock][-1] < 30:
-                embed = discord.Embed(
-                    title=stock,
-                    description=('Buy ' + stock + ', RSI: ' + str(int(stocks[stock][-1]))),
-                    colour=discord.Colour.purple()
-                )
-                await message.channel.send(embed=embed, file=discord.File("pictures/" + stock + ".png"))
+    if message.content.startswith('$help'):
+        embed1 = discord.Embed(
+        title= 'Commands',
+        description= str('$Stocklist for a list of multiple stocks' + '\n' + '$stock (name) for info on single stock'),
+        colour=discord.Colour.dark_gray())
+        await message.channel.send(embed=embed1)
 
-                # if message.content.startswith('$stocks'):
-                #     for stock in stocks:
-                #         if stocks[stock][-1] > 70:
-                #             await message.channel.send('Sell ' + stock + ', RSI: ' + str(int(stocks[stock][-1])))
-                #             await message.channel.send(file=discord.File("pictures/" + stock + ".png"))
 
-                #         elif stocks[stock][-1] < 30:
-                #             await message.channel.send('Buy ' + stock + ', RSI: ' + str(int(stocks[stock][-1])))
-                #             await message.channel.send(file=discord.File("pictures/" + stock + ".png"))
-        
-        client.run('token')
+#Höger klicka på text kanal sedan copy channel ID
+
+client.run("token")
+
